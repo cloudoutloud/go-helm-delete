9c1de31d0475dc8bfaec0afb4eb87eef515d48d7
cloudoutloud <39462069+cloudoutloud@users.noreply.github.com>
2025-04-06 10:48:01 +0900

    Update

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	action "helm.sh/helm/v3/pkg/action"
@@ -20,12 +22,33 @@ func main() {
 	namespace := os.Getenv("HELM_NAMESPACE")
 	if namespace == "" {
 		log.Fatalf("HELM_NAMESPACE environment variable is not set")
+	} else {
+		fmt.Printf("Using namespace: %s\n", namespace)
+	}
+
+	thresholdEnv := os.Getenv("THRESHOLD_HOURS")
+	thresholdHours := 24 // Default to 24 hour
+	if thresholdEnv != "" {
+		parsedThreshold, err := strconv.Atoi(thresholdEnv)
+		if err != nil {
+			log.Fatalf("Invalid value for THRESHOLD_HOURS: %v", err)
+		}
+		thresholdHours = parsedThreshold
+	}
+
+	exemptReleasesEnv := os.Getenv("EXEMPT_RELEASES")
+	var exemptReleases []string
+	if exemptReleasesEnv != "" {
+		exemptReleases = strings.Split(exemptReleasesEnv, ",")
+		fmt.Printf("Exempt releases: %v\n", exemptReleases)
+	} else {
+		fmt.Println("No exempt releases specified in EXEMPT_RELEASES")
 	}
 
 	settings.SetNamespace(namespace)
 
 	listAction := action.NewList(actionConfig)
-	listAction.AllNamespaces = false // Limit to a specific namespace
+	listAction.AllNamespaces = false
 	listAction.StateMask = action.ListAll
 
 	releases, err := listAction.Run()
@@ -34,10 +57,15 @@ func main() {
 	}
 
 	now := time.Now()
-	threshold := now.Add(-6 * time.Hour) // 6 hours threshold
+	threshold := now.Add(-time.Duration(thresholdHours) * time.Hour)
 	deleteAction := action.NewUninstall(actionConfig)
 
 	for _, r := range releases {
+		if isExempted(r.Name, exemptReleases) {
+			fmt.Printf("Skipping exempted release %s\n", r.Name)
+			continue
+		}
+
 		if r.Info.LastDeployed.Time.Before(threshold) {
 			fmt.Printf("Deleting release %s in namespace %s (Deployed: %v)\n", r.Name, r.Namespace, r.Info.LastDeployed)
 			_, err := deleteAction.Run(r.Name)
@@ -47,8 +75,18 @@ func main() {
 				fmt.Printf("Successfully deleted release %s\n", r.Name)
 			}
 		} else {
-			fmt.Printf("No releases to delete")
+			fmt.Printf("No releases to delete\n")
 		}
 	}
-	fmt.Printf("Script Done\n")
+	fmt.Printf("Finished..\n")
+}
+
+// Helper function to check if a release name is in the exempt list
+func isExempted(name string, exemptReleases []string) bool {
+	for _, exempt := range exemptReleases {
+		if name == exempt {
+			return true
+		}
+	}
+	return false
 }
